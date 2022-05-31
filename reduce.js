468a0778e5dfb568bb9583c86e3986829046ca24
JD Ribstein <jdrib@hotmail.fr>
2022-05-31 16:56:10 +0200

    new Frontend Masters file using reduce

---
+++ reduce.js
@@ -0,0 +1,63 @@
+const newDevelopment = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { "dining room": true },
+      { "billiard room": false },
+      { library: true },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": true },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: true },
+      { "dining room": false },
+      { "billiard room": true },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: true },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": true },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+
+// want to get a list of rooms where noone was present, i.e. where the room evaluates to false in the rooms aray of every character
+function notInRoom(char) {
+  char.rooms.reduce((temp, current) => {
+    for (const prop in current) {
+      if (!current[prop]) {
+        temp.push(prop);
+      }
+    }
+  }, []);
+}
+
+notInRoom(newDevelopment[0]);
